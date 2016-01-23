38b242911f601bd5a13d1601e3b6d878845935c8
Mathew Kurian <bluejamesbond@gmail.com>
2016-01-23 15:41:19 -0600

    Babel resolver fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require('babel-register');
 
-var server = require('../server').default;
+var server = require('./server').default;
 var port = parseInt(process.env.BRANCHOFF_PORT) || process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`Listening on ${port}`));
