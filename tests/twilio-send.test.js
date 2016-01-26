5ef3a6a3d3159e7e9a21cb7c759e13ed67e93b14
Kevin Fu <kevin.fu808@gmail.com>
2016-01-26 03:14:39 -0600

    got a single test message to work correctly

--- tests/twilio-send.test.js
+++ tests/twilio-send.test.js
@@ -3,8 +3,6 @@ import expect from 'expect.js'
 
 describe('Twilio Send', ()=> {
   it('using test SMS should work successfully', done => {
-    expect(sendTestSMS("2149664948", "This is a test message to be sent to a using the test credentials to a phone number.")).to.not.throwException();
-    done();
-
+    sendTestSMS("2149664948", "This is a test message to be sent to a using the test credentials to a phone number.");
   });
 });
