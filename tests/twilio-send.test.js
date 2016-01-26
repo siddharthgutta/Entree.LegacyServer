e94f8e85557de46d18a1d6b19309a83f95873e76
Mathew Kurian <bluejamesbond@gmail.com>
2016-01-26 09:45:34 -0600

    Fixing tests

--- tests/twilio-send.test.js
+++ tests/twilio-send.test.js
@@ -1,8 +1,20 @@
 import {sendStandardSMS, sendTestSMS} from "../libs/twilio/sms";
 import expect from 'expect.js'
+import {initScribe} from '../bootstrap'
+import moment from 'moment';
+
+const console = initScribe(true); // set to true
+
+console.tag(__filename).log("Starting tests");
 
 describe('Twilio Send', ()=> {
   it('using test SMS should work successfully', done => {
+<<<<<<< HEAD
     sendTestSMS("2149664948", "This is a test message to be sent to a using the test credentials to a phone number.");
+=======
+    const txt = "TEST SMS " + moment().format("h:mm A");
+    expect(sendTestSMS).withArgs("2149664948", txt).to.not.throwException();
+    done();
+>>>>>>> Fixing tests
   });
 });
