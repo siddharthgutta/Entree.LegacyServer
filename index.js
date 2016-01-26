c1590d3ccc6cc2894c9e0584130982b940d772b0
Mathew Kurian <bluejamesbond@gmail.com>
2016-01-25 23:04:33 -0600

    Bootstrapping

--- index.js
+++ index.js
@@ -3,54 +3,8 @@
 // precompile on production
 require('babel-register');
 
-var Scribe = require('scribe-js');
-var config = require('config');
-var cluster = require('cluster');
+var Bootstrap = require('./bootstrap');
 
-var PORT = parseInt(process.env.BRANCHOFF_PORT) || process.env.PORT || 3000;
-var SOCKET_PORT = 50000 + Number(process.env.pm_id) || PORT;
-var NAME = process.env.BRANCHOFF_BRANCH || PORT;
-
-var console = new Scribe(NAME, {
-  name: 'Entree',
-  mongoUri: 'mongodb://localhost/scribe',
-  basePath: 'scribe/',
-  socketPort: SOCKET_PORT,
-  nwjs: {
-    buildDir: `${__dirname}/../public/native`
-  },
-  web: {
-    router: {
-      username: 'build',
-      password: 'build',
-      authentication: true,
-      sessionSecret: 'scribe-session',
-      useBodyParser: true,
-      useSession: true
-    },
-    client: {
-      port: PORT,
-      socketPorts: [SOCKET_PORT],
-      exposed: {
-        all: {label: 'all', query: {expose: {$exists: true}}},
-        error: {label: 'error', query: {expose: 'error'}},
-        express: {label: 'express', query: {expose: 'express'}},
-        info: {label: 'info', query: {expose: 'info'}},
-        log: {label: 'log', query: {expose: 'log'}},
-        warn: {label: 'warn', query: {expose: 'warn'}},
-        trace: {label: 'trace', query: {expose: 'trace'}},
-        timing: {label: 'time', query: {expose: 'timing'}},
-        user: {label: 'user', query: {'transient.tags': {$in: ['USER ID']}}}
-      }
-    }
-  },
-  native: {},
-  debug: false
-});
-
-console.persistent('tags', [NAME, process.env.NODE_ENV || "LOCAL_BUILD?"]);
-console.override();
-
-var server = require('./server').default;
-
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+Bootstrap.initScribe();
+Bootstrap.initDatabase();
+Bootstrap.initServer();
