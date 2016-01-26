ad6bba0776b5192a5e66bf99b1d8edbdf5bc41e4
Kevin Fu <kevin.fu808@gmail.com>
2016-01-26 02:11:23 -0600

    expecting not to throw an exception

--- tests/twilio-send.test.js
+++ tests/twilio-send.test.js
@@ -3,7 +3,7 @@ import expect from 'expect.js'
 
 describe('Twilio Send', ()=> {
   it('using test SMS should work successfully', done => {
-    expect(sendTestSMS("2149664948", "This is a test message to be sent to a using the test credentials to a phone number.")).to.be(true);
+    expect(sendTestSMS("2149664948", "This is a test message to be sent to a using the test credentials to a phone number.")).to.not.throwException();
     done();
 
   });
