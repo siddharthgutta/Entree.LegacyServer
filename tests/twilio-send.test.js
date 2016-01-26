006ffa87db926ac9cc9f33b2ff42a3401943389d
Kevin Fu <kevin.fu808@gmail.com>
2016-01-26 01:58:51 -0600

    unfinished tests for twilio

---
+++ tests/twilio-send.test.js
@@ -0,0 +1,10 @@
+import {sendStandardSMS, sendTestSMS} from "../libs/twilio/sms";
+import expect from 'expect.js'
+
+describe('Twilio Send', ()=> {
+  it('using test SMS should work successfully', done => {
+    expect(sendTestSMS("2149664948", "This is a test message to be sent to a using the test credentials to a phone number.")).to.be(true);
+    done();
+
+  });
+});
