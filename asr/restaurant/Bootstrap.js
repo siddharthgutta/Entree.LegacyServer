e8fa8ff88f8e958cf3664f00e9855af236034323
Mathew Kurian <bluejamesbond@gmail.com>
2016-02-02 08:58:26 -0600

    Prevent ghost click - RELEASE

--- asr/restaurant/Bootstrap.js
+++ asr/restaurant/Bootstrap.js
@@ -4,7 +4,7 @@ import {render} from 'react-dom';
 import tapInject from 'react-tap-event-plugin';
 
 if (/iPad|iPhone|iPod/.test(navigator.platform)) {
-  tapInject();
+  tapInject({shouldRejectClick: () => true});
 }
 
 render(<App />, document.getElementById('app'));
